96fffb98ac5f710b8449fffe108e5fa71b7e3dfa
TANMAY KUDKAR <kudkartanmay25@gmail.com>
2025-10-11 16:29:11 +0530

    Initial commit

---
+++ backend/src/main/java/com/queuefree/employee/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.queuefree.employee.controller;
+
+import com.queuefree.employee.model.Employee;
+import com.queuefree.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // Additional endpoints for GET by id, PUT, and DELETE can be added here.
+}
---
+++ backend/src/main/java/com/queuefree/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.queuefree.employee.repository;
+
+import com.queuefree.employee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
