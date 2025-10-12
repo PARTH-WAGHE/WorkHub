eaa0739dffd0d6c6653d663d4491fffb1b094991
TANMAY KUDKAR <kudkartanmay25@gmail.com>
2025-10-12 12:38:40 +0530

    Adding Tailwind CSS Support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // ensure relative asset paths in production
   plugins: [react()],
   server: {
     port: 5173,
